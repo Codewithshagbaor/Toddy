ec4a0e57bcc6cffc02a26bad6cc40b9df5d2658c
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-11 21:52:37 +0100

    Delete Base directory

--- Base/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-  todo = models.CharField(max_length=10000)
-  uid = models.CharField(max_length=1000)
-
-  def __str__(self):
-    return self.todo
--- Base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('', views.index, name="index"),
-  path('delete/<str:uid>', views.delete_view, name="delete"),
-]
